c02da9dc97b6d45e19c2c0be5870b26680da7d9b
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-04-29 08:58:49 +0200

    changed config style

--- .config/wireplumber/main.lua.d/51-TU116HDMIOUT-disable.lua
+++ .config/wireplumber/main.lua.d/51-TU116HDMIOUT-disable.lua
@@ -1,15 +1,13 @@
-monitor.alsa.rules = [
-  {
-    matches = [
-      {
-        device.name = "alsa_card.pci-0000_0a_00.1" 
-      }
-    ]
-    actions = {
-      update-props = {
-	 node.description = "GTX 1660 HDMI"
-         device.disabled = true
-      }
-    }
-  }
-]
+rule = {
+  matches = {
+    {
+      { "device.name", "equals", "alsa_card.pci-0000_0a_00.1" },
+    },
+  },
+  apply_properties = {
+    ["node.description"] = "Front Input",
+    ["device.disabled"] = true,
+  },
+}
+
+table.insert(alsa_monitor.rules, rule)
