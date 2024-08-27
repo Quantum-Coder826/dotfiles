0783de92f619a24c4fe748e14444635348131bb1
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-08-27 09:24:55 +0200

    added extra startup daemon

--- .config/qtile/scripts/startup.sh
+++ .config/qtile/scripts/startup.sh
@@ -5,11 +5,12 @@
 (/usr/bin/lxqt-policykit-agent &) # polkit daemon
 (qpwgraph &)
 (easyeffects &)
+(greenclip daemon &)
 
 ## Screen setup for xorg
 xrandr --output HDMI-0 --mode 1920x1080 --rate 75 --primary
 xrandr --output DVI-D-0 --mode 1920x1080 --left-of HDMI-0
 
 # my startup apps
-(discord &) # add --start-minimized when need to start in tray
+(/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=com.discordapp.Discord com.discordapp.Discord &) # add --start-minimized when need to start in tray
 (steam &)
