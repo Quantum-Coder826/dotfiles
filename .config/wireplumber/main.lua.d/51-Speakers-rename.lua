c80a3e4a4336f7cbe45ab544d9037ddfdb00fda1
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-09-08 20:04:51 +0200

    Fixed wireplumber scripts for names

--- .config/wireplumber/main.lua.d/51-Speakers-rename.lua
+++ .config/wireplumber/main.lua.d/51-Speakers-rename.lua
@@ -1,11 +1,12 @@
 rule = {
   matches = {
     {
-      { "node.name", "equals",  "alsa_output.pci-0000_0c_00.4.iec958-stereo" },
+      { "node.name", "equals",  "alsa_output.pci-0000_0c_00.4.analog-stereo" },
     },
   },
   apply_properties = {
     ["node.description"] = "Speakers",
+    ["node.nick"] = "Logi Z702",
   },
 }
 
--- .config/wireplumber/main.lua.d/51-headset-rename.lua
+++ .config/wireplumber/main.lua.d/51-headset-rename.lua
@@ -1,11 +1,12 @@
 rule = {
   matches = {
     {
-      { "node.name", "equals",  "alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.iec958-stereo" },
+      { "node.name", "equals",  "alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-stereo" },
     },
   },
   apply_properties = {
     ["node.description"] = "Headset",
+    ["node.nick"] = "HyperX Cloud II",
   },
 }
 
