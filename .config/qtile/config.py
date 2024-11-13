8d934c6ace7dbdadb843b7c495bb652fd069c42c
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-11-13 09:51:34 +0100

    Set warp_cursor to true

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -309,7 +309,7 @@ floating_types = ["notification", "toolbar", "splash", "dialog"]
 
 follow_mouse_focus = True
 bring_front_click = False
-cursor_warp = False
+cursor_warp = True 
 floating_layout = layout.Floating(float_rules=[ # Match chases to make window
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
