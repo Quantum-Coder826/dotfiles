f7e5bc7ccd17a8aeb2bc26a7fd6d653cdd819656
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-07-06 10:11:03 +0200

    Changed over to discord package

--- .config/qtile/scripts/startup.sh
+++ .config/qtile/scripts/startup.sh
@@ -8,13 +8,11 @@ xrandr --output DVI-D-0 --left-of HDMI-0
 (/usr/lib/polkit-kde-authentication-agent-1 &)
 (XDG_MENU_PREFIX=arch- kbuildsycoca6 &)
 
-#(/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=qpwgraph --file-forwarding org.rncbc.qpwgraph &)
 (/usr/bin/helvum &)
 (com.github.wwmm.easyeffects &)
 (/home/qbyte/.local/bin/greenclip daemon &)
-#(/home/qbyte/.config/qtile/scripts/bin/python3 /home/qbyte/.config/qtile/scripts/utils.py &)
 (/bin/bash /home/qbyte/.config/qtile/scripts/net_up.sh &)
 
 # my startup apps
-(/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=com.discordapp.Discord com.discordapp.Discord &) # add --start-minimized when need to start in tray
+(/usr/bin/discord &) # add --start-minimized when need to start in tray
 (steam &)
