049adb16f749b19419896b130997b9386be48a08
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-12-19 16:16:55 +0100

    changed over to bash script for gpu monitoring

--- dot-config/waybar/scripts/custom-gpu.sh
+++ dot-config/waybar/scripts/custom-gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 amd-smi metric --json | jq --unbuffered --compact-output '.[0] | {text: "󰾲 \(.usage.gfx_activity.value)%", 
-tooltip: "󰔏\(.temperature.hotspot.value)󰔄\r󰈐 \(.fan.usage.value)󰏰\r󱩗 \(.power.socket_power.value)W"}' 
+tooltip: "󰔏 \(.temperature.hotspot.value)󰔄\r󰈐 \(.fan.usage.value)󰏰\r󱩗 \(.power.socket_power.value)W"}' 
 
