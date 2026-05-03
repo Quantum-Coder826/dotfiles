ad58e0d318cf944ac3c47dc4c50c327ee01b6042
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-05-03 08:24:42 +0200

    Changed folder locations

--- scripts/screenshooter.sh
+++ scripts/screenshooter.sh
@@ -3,9 +3,9 @@
 ans=$(echo -e " monitor\n window\n region\n active\n cancel" | rofi -dmenu -format i:s)
 
 case "${ans:0:1}" in
-    0) hyprshot -s -m output;;
-    1) hyprshot -s -m window;;
-    2) hyprshot -s -m region;;
-    3) hyprshot -s -m active;;
+    0) hyprshot -o ~/Pictures/Screenshots -s -m output;;
+    1) hyprshot -o ~/Pictures/Screenshots -s -m window;;
+    2) hyprshot -o ~/Pictures/Screenshots -s -m region;;
+    3) hyprshot -o ~/Pictures/Screenshots -s -m active;;
     4) exit;;
 esac
