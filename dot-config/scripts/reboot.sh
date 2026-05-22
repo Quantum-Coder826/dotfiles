9b69f7d671425fc8cbf8c9620a73cd7e8124f7a6
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-05-22 09:20:28 +0200

    Removed uneeded chr

--- dot-config/scripts/reboot.sh
+++ dot-config/scripts/reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # prompt that will ask to cancle, or power off reboot the system
 
-ans=$(echo -e "cancel\nreboot\nsoft reboot\nreboot to UEFI" | rofi -dmenu -format i -p "Reboot:")
+ans=$(echo -e "cancel\nreboot\nsoft reboot\nreboot to UEFI" | rofi -dmenu -format i -p "Reboot")
 
 case $ans in
     0) exit;;
