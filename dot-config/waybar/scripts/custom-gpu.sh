f1b090a33d0b4d69d9963a66e4b1a73ce5230c2d
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-06-15 11:18:21 +0200

    Formatting

--- dot-config/waybar/scripts/custom-gpu.sh
+++ dot-config/waybar/scripts/custom-gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-amd-smi metric --json | jq --unbuffered --compact-output '.gpu_data[0] | {text: "󰾲 \(.usage.gfx_activity.value)%", 
-tooltip: "󰔏 \(.temperature.hotspot.value)󰔄\r󰈐 \(.fan.usage.value)󰏰\r󱩗 \(.power.socket_power.value)W"}' 
+amd-smi metric --json | jq --unbuffered --compact-output '.gpu_data[0] | {text: "󰾲\(.usage.gfx_activity.value)󱉸", 
+tooltip: "󰔏 \(.temperature.hotspot.value)󰔄\r󰈐 \(.fan.usage.value)󰏰\r󱩗 \(.power.socket_power.value)w"}' 
 
